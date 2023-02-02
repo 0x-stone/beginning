976a4fe5e183dd4a70cd7af4190805d037657fcc
stone <Lau.Stone@vce.com>
2023-02-02 13:41:43 +0800

    feat: new staking

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -29,3 +29,21 @@ contract Stakeing is ReentrancyGuard {
 
 	function withdraw(address ERC20Address, uint256 Amount) external nonReentrant {}
 }
+
+contract newStakeing is ReentrancyGuard {
+	mapping(bytes32 => uint256) Vault;
+
+	event Dposited(address, address, uint256);
+
+	using SafeMath for uint256;
+	using SafeERC20 for IERC20;
+
+	function deposit(address ERC20Address, uint256 Amount) external nonReentrant {
+		address sender = msg.sender;
+		bytes32 hash = keccak256(abi.encodePacked(ERC20Address, sender));
+		uint256 _balance = Vault[hash];
+		Vault[hash] = _balance.add(Amount);
+		IERC20(ERC20Address).safeTransferFrom(sender, address(this), Amount);
+		emit Dposited(ERC20Address, sender, Amount);
+	}
+}
